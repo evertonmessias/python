5e0b38c405cb154fb03ab65103553dab03a7766f
evertonmessias <everton.messias@gmail.com>
2018-03-16 16:28:05 -0300

    16-03-2018_16:28:05

--- teste02.py
+++ teste02.py
@@ -1,8 +1,9 @@
 #Equação do 2º Grau
+from math import sqrt, pow
 a=float(input("Digite o Valor A:\n"))
 b=float(input("Digite o Valor B:\n"))
 c=float(input("Digite o Valor C:\n"))
-delta=float((b**2)-(4*a*c))
-x1=float((((-1)*b)+(delta**(1/2)))/(2*a))
-x2=float((((-1)*b)-(delta**(1/2)))/(2*a))
-print("Delta={}, x1={}, x2={} ".format(delta,x1,x2))
+delta=float((pow(b,2))-(4*a*c))
+x1=float((((-1)*b)+(sqrt(delta)))/(2*a))
+x2=float((((-1)*b)-(sqrt(delta)))/(2*a))
+print("Delta={}, x1={:.2f}, x2={:.2f} ".format(delta,x1,x2))
