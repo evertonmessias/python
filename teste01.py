1542372c91e4f0b8dc0990b0ec3451963eab6a33
evertonmessias <everton.messias@gmail.com>
2018-03-15 21:31:46 -0300

    15-03-2018_21:31:46

---
+++ teste01.py
@@ -0,0 +1,7 @@
+nome=str(input("nome?\n"))
+idade=int(input("idade?\n"))
+n1=float(input("nota1?\n"))
+n2=float(input("nota2?\n"))
+media=float((n1+n2)/2)
+print("Nome:{}, Idade:{}, e media:{} ".format(nome,idade,media))
+print(type(nome));print(type(idade));print(type(media))
