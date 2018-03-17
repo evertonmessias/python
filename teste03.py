136504c2ee9cc40e2c84c6bb71f81306b60cb7dc
evertonmessias <everton.messias@gmail.com>
2018-03-16 21:44:24 -0300

    16-03-2018_21:44:24

---
+++ teste03.py
@@ -0,0 +1,20 @@
+frase=str("Estou estudando Linguagem Python")
+frase2=str("   Estou com texto bagunçado     ")
+print("01 - ",frase[:15])
+print("02 - ",len(frase))
+print("03 - ",frase.count('E'))
+print("04 - ",frase.find("estudando"))
+print("05 - ",'Python' in frase)
+print("06 - ",frase.replace('Python','Android'))
+print("07 - ",frase.upper())
+print("08 - ",frase.lower())
+print("09 - ",frase.capitalize())
+print("10 - ",frase.title())
+print("11 - ",frase2.strip())
+print("12 - ",frase.split())
+
+
+
+
+
+
