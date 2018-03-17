63ec827f89d69fe32485bc2a7ad8716c756865fd
evertonmessias <everton.messias@gmail.com>
2018-03-17 18:14:42 -0300

    17-03-2018_18:14:42

--- teste02.py
+++ teste02.py
@@ -1,14 +1,24 @@
 #Equação do 2º Grau
+import os
 from math import sqrt, pow
-a=float(input("Digite o Valor A:\n"))
-b=float(input("Digite o Valor B:\n"))
-c=float(input("Digite o Valor C:\n"))
-delta=float((pow(b,2))-(4*a*c))
-if delta > 0:
-    x1=float((((-1)*b)+(sqrt(delta)))/(2*a))
-    x2=float((((-1)*b)-(sqrt(delta)))/(2*a))
-    print("\33[0;34mDelta={}, x1={:.2f}, x2={:.2f} ".format(delta,x1,x2))
-else:
-    print("\33[0;31mNão existe raiz real")
-
+r = str('s')
+while r == 's':
+    os.system("clear")
+    print("\33[1;34m\n**Equacao do 2ºGrau**\n")    
+    a=float(input("\33[0;34mDigite o Valor A:\n"))
+    b=float(input("\33[0;34mDigite o Valor B:\n"))
+    c=float(input("\33[0;34mDigite o Valor C:\n"))
+    delta=float((pow(b,2))-(4*a*c))
+    if a == 0:
+        for c in range(2):
+            print("\33[1;31m\nO valor A não pode ser zero!\n")
+    elif delta > 0:
+        x1=float((((-1)*b)+(sqrt(delta)))/(2*a))
+        x2=float((((-1)*b)-(sqrt(delta)))/(2*a))
+        print("\33[1;32m\nDelta={}, x1={:.2f}, x2={:.2f}\n".format(delta,x1,x2))
+    else:
+        print("\33[1;31m\nNão existe raiz real\n")
+    r = str(input("\33[1;32m\nDeseja continuar? (s/n)"))
+print ("\33[1;32m\nFIM\n")    
+        
     
